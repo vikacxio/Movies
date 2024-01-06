9ba02ca85dee38bc863e5d2657b26d2ce5566b6b
vikacxio <vikacxio@gmail.com>
2024-01-06 15:34:12 +0530

    movieRecc

---
+++ moviefrontend/src/App.js
@@ -0,0 +1,72 @@
+
+import './App.css';
+import api from './api/axiosConfig'
+import { useState, useEffect } from 'react';
+import Layout from './component/Layout';
+import { Route, Routes } from 'react-router-dom';
+import Home from './component/home/Home';
+import Header from './component/header/Header';
+import Trailer from './component/trailer/Trailer';
+import Reviews from './component/reviewForm/reviews/Reviews';
+import PageNotFound from './component/pageNotFound/PageNotFound';
+
+function App() {
+  const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState();
+
+  const getMovies = async () => {
+
+    try {
+      const response = await api.get("api/v1/movies");
+      setMovies(response.data);
+
+    } catch (err) {
+      console.log(err);
+    }
+
+  }
+
+  const getMovieData = async (movieId) => {
+
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+
+      setReviews(singleMovie.reviewIds);
+
+
+    }
+    catch (error) {
+      console.error(error);
+    }
+
+  }
+  useEffect(() => {
+
+    document.title = "movie"
+    getMovies();
+  }, [])
+
+  console.log(movies)
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />}> </Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
+
+
+        </Route>
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ moviefrontend/src/component/pageNotFound/PageNotFound.js
@@ -0,0 +1,6 @@
+const PageNotFound = () => {
+    return (
+        <div><h1>404,Page not found</h1></div>
+    )
+}
+export default PageNotFound;
